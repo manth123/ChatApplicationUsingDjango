8e3b1785732aef5fccc61705eb15a4b32f96b645
Manth123 <77846329+manth123@users.noreply.github.com>
2021-05-11 18:29:11 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,14 @@
-"""ChatApp URL Configuration
+from django.contrib.auth.views import logout
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chat.urls')),
+    path('', views.index, name='index'),
+    path('chat/', views.chat_view, name='chats'),
+    path('chat/<int:sender>/<int:receiver>/', views.message_view, name='chat'),
+    path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
+    path('api/messages/', views.message_list, name='message-list'),
+    path('logout/', logout, {'next_page': 'index'}, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
